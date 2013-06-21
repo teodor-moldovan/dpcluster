36b2588cf5aa140746ba59c5c77c741ebbc10e7a
Teodor Mihai Moldovan <moldovan@cs.berkeley.edu>
2013-06-21 15:06:48 -0700

    changed repo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='dpcluster',
-      version='0.103',
+      version='0.104',
       description='dpcluster is a package for grouping together (clustering) vectors. It automatically chooses the number of clusters that fits the data best based on the underlying Dirichlet Process mixture model.',
       long_description=readme(),
       classifiers=[
@@ -15,7 +15,7 @@ setup(name='dpcluster',
         'Development Status :: 2 - Pre-Alpha'
         ],
       url='http://dpcluster.readthedocs.org/',
-      download_url='http://bitbucket.org/moldovan/dpcluster',
+      download_url='http://github.com/teodor-moldovan/dpcluster',
       author='Teodor Mihai Moldovan',
       author_email='moldovan@cs.berkeley.edu',
       packages=['dpcluster'],
